125549448cb1ec03e097a09a95d076c596a97fd8
Aklongmuir <43087101+Aklongmuir@users.noreply.github.com>
2018-10-07 08:58:47 +1100

    Google Analytics Fix

---
+++ www/google-analytics.js
@@ -0,0 +1,25 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <meta http-equiv="Content-Style-Type" content="text/css">
+  <title></title>
+  <meta name="Generator" content="Cocoa HTML Writer">
+  <meta name="CocoaVersion" content="1504.83">
+  <style type="text/css">
+    p.p1 {margin: 0.0px 0.0px 0.0px 0.0px; font: 12.0px Helvetica}
+    p.p2 {margin: 0.0px 0.0px 0.0px 0.0px; font: 12.0px Helvetica; min-height: 14.0px}
+  </style>
+</head>
+<body>
+<p class="p1">&lt;!-- Global site tag (gtag.js) - Google Analytics --&gt;</p>
+<p class="p1">&lt;script async src="https://www.googletagmanager.com/gtag/js?id=UA-78064099-2"&gt;&lt;/script&gt;</p>
+<p class="p1">&lt;script&gt;</p>
+<p class="p1"><span class="Apple-converted-space">  </span>window.dataLayer = window.dataLayer || [];</p>
+<p class="p1"><span class="Apple-converted-space">  </span>function gtag(){dataLayer.push(arguments);}</p>
+<p class="p1"><span class="Apple-converted-space">  </span>gtag('js', new Date());</p>
+<p class="p2"><br></p>
+<p class="p1"><span class="Apple-converted-space">  </span>gtag('config', 'UA-78064099-2');</p>
+<p class="p1">&lt;/script&gt;</p>
+</body>
+</html>
